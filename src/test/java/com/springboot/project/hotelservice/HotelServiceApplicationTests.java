3c4d9a39d136b99e62b9b4880f7734c1ab88b010
Suresh_Prabha <suresh.uday92@gmail.com>
2021-06-21 19:15:04 +0530

    Changes

--- src/test/java/com/springboot/project/hotelservice/HotelServiceApplicationTests.java
+++ src/test/java/com/springboot/project/hotelservice/HotelServiceApplicationTests.java
@@ -1,62 +1,32 @@
 package com.springboot.project.hotelservice;
 
-import com.springboot.project.hotelservice.entity.Hotel;
-import com.springboot.project.hotelservice.entity.Room;
-import com.springboot.project.hotelservice.entity.RoomTypes;
-import org.junit.jupiter.api.Assertions;
+import com.springboot.project.hotelservice.controller.HotelController;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Arrays;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
 class HotelServiceApplicationTests {
 
     @Autowired
-    private TestRestTemplate testRestTemplate;
+    HotelController hotelController;
 
     @BeforeEach
-    public void initMocks() {
-        MockitoAnnotations.initMocks(this);
+    void setup() {
+        RestAssuredMockMvc.standaloneSetup(hotelController);
     }
 
     @Test
-    void testCreateHotel() {
-        Room room = Room.builder().id(1).roomType(RoomTypes.KING_BED).build();
-        Hotel hotel = Hotel.builder().hotelId("12c9f1e4a1a04878860b588f42d37a6d")
-                .hotelName("Name").roomDetails(Arrays.asList(room)).build();
-        HttpEntity<Hotel> entity = new HttpEntity<>(hotel);
-        ResponseEntity<String> responseEntity = testRestTemplate.exchange("/hotels", HttpMethod.POST, entity,
-                String.class);
-        Assertions.assertEquals(responseEntity.getStatusCodeValue(),HttpStatus.CREATED.value());
-    }
-
-    @Test
-    void testCreateHotelBadRequest() {
-        Hotel hotel = Hotel.builder().build();
-        HttpEntity<Hotel> entity = new HttpEntity<>(hotel);
-        ResponseEntity<String> responseEntity = testRestTemplate.exchange("/hotels", HttpMethod.POST, entity,
-                String.class);
-        Assertions.assertEquals(responseEntity.getStatusCodeValue(),HttpStatus.BAD_REQUEST.value());
-    }
-
-    @Test
-    void testDeleteHotel() {
-        testRestTemplate.execute("/hotels/12c9f1e4a1a04878860b588f42d37a6d",
-                HttpMethod.DELETE,null, null);
-        ResponseEntity<String> responseEntity = testRestTemplate.getForEntity("/hotels/12c9f1e4a1a04878860b588f42d37a6d", String.class);
-        Assertions.assertEquals(responseEntity.getStatusCodeValue(),HttpStatus.INTERNAL_SERVER_ERROR.value());
+    public void testController(){
+        Assert.assertNull(hotelController);
     }
 
 }
---
+++ src/test/resources/contracts/package/shouldReturnHotelWhenPathParamIsID.groovy
@@ -0,0 +1,15 @@
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    description "should return Hotel when ID input is given"
+    request{
+        method GET()
+        url("/hotels/f387022e93774015aede2d4f5453a2aa") {
+
+        }
+    }
+    response {
+        body($(consumer(file("response.json"))))
+        status 200
+    }
+}
